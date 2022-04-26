11d03bad2ad11974a0eaaf71415e0bb8d3a06339
Matt Bryant <brybrant@gmail.com>
2022-04-26 22:27:39 +0100

    Prevent wrapping navigation items on smaller screens

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
-    <nav class="main-nav" aria-label="Navigation" itemscope itemtype="http://www.schema.org/SiteNavigationElement">
-      <router-link to="/" active-class="active" itemprop="url" exact>Home</router-link>
-      <router-link to="/commands" active-class="active" itemprop="url">Commands</router-link>
-      <router-link to="/faq" active-class="active" itemprop="url">F.A.Q</router-link>
-      <a href="https://discord.gg/VyQTrwP" target="_blank" itemprop="url">Support</a>
+    <nav class="flexbox" aria-label="Navigation" itemscope itemtype="http://www.schema.org/SiteNavigationElement">
+      <div class="flexbox__item">
+        <router-link to="/" active-class="active" itemprop="url" exact>Home</router-link>
+      </div>
+      <div class="flexbox__item">
+        <router-link to="/commands" active-class="active" itemprop="url">Commands</router-link>
+      </div>
+      <div class="flexbox__item">
+        <router-link to="/faq" active-class="active" itemprop="url">F.A.Q</router-link>
+      </div>
+      <div class="flexbox__item">
+        <a href="https://discord.gg/VyQTrwP" target="_blank" itemprop="url">Support</a>
+      </div>
     </nav>
 
     <router-view :invite-link='"https://discord.com/api/oauth2/authorize?client_id=310050883100737536&permissions=939879488&scope=bot%20applications.commands"'/>
