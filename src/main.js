18f0f6c4ca9cf0330d2ff53a6b0f9e0c0643e341
Matt Bryant <brybrant@gmail.com>
2022-05-10 23:08:30 +0100

    Created a "twoOrphans" vue filter, to force a minimum of two orphaned words at the end of blocks of text to which the filter is applied.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+Vue.filter("twoOrphans",function(text) {
+  // Replace the last whitespace character with a non-breaking space to force a
+  // minimum of two orphan words in a given block of text
+  return text.replace(/\s+[\w")]+$/, function(x) {
+    return x.replace(/\s+/,"\xa0");// non-breaking space (javascript entity)
+  });
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
