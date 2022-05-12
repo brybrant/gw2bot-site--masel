63c06c98fadebcabfa9d4256a737a0e360363613
Matt Bryant <brybrant@gmail.com>
2022-05-12 12:00:27 +0100

    Reduced and simplified Cmd.vue component code with superior logic. Moved command arguments code to new seperate CmdArgs.vue component. These two changes create more efficient code with much less repetition. === Fixed line heights and improved light theme colours (dark theme soon tm) General appearance improvements

--- src/Cmd.vue
+++ src/Cmd.vue
@@ -1,69 +1,46 @@
 <template>
   <li class="command">
-    <template v-if="(!command.subcommands)&&(!command.args)">
-      <h3 class="command__name"><span>{{ command.name }}</span></h3>
-      <p class="command__desc">{{ command.desc | twoOrphans }}</p>
-    </template>
+    <div v-if="command.args" class="tooltip" :class="{active: command.active}" :id="command.name+'-tooltip'">
+      <cmdArgs :command="command"/>
+    </div>
 
-    <template v-else-if="command.args">
-      <div class="tooltip" :class="{active: command.active}" :id="command.name+'-tooltip'" role="tooltip">
-        <ul class="arg-list">
-          <li class="arg" v-for="arg in command.args" :key="arg.id">
-            <p class="arg__name">{{ arg.name }} <span>(<template v-if="arg.required">required</template><template v-else>optional</template>)</span></p>
-            <p class="small-text" v-html="$options.filters.twoOrphans(arg.desc)+(arg.options?':':'')"></p>
-            <ul class="option-list" v-if="arg.options">
-              <li class="option small-text" v-for="option in arg.options" :key="option.id" v-html="$options.filters.twoOrphans(option)"></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-      <h3 class="command__name">
-        <span>{{ command.name }}</span> <button @click="toggleActive(command)" class="args-button" :class="{active: command.active}" :aria-describedby="command.name+'-tooltip'">+{{ command.args.length }}</button>
-      </h3>
-      <p class="command__desc">{{ command.desc | twoOrphans }}</p>
-    </template>
+    <h3 class="command__name">
+      <template v-if="!command.subcommands">
+        <span>{{ command.name }}</span>
+        <button v-if="command.args" @click="toggleActive(command)" class="args-button" :class="{active: command.active}" :aria-controls="command.name+'-tooltip'" :aria-expanded="command.active?'true':'false'" :title="command.active?'Hide command arguments':'Show command arguments'" v-html="'+'+command.args.length"></button>
+      </template>
+      <template v-else-if="command.subcommands">
+        <button @click="toggleActive(command)" class="command-button" :class="{active: command.active}" :aria-controls="command.name+'-subcommands'" :aria-expanded="command.active?'true':'false'" :title="command.active?'Hide subcommands':'Show subcommands'">
+          <span>{{ command.name }}</span>
+        </button>
+      </template>
+    </h3>
+    <p class="command__desc">{{ command.desc | twoOrphans }}</p>
 
-    <template v-else-if="command.subcommands">
-      <h3 class="command__name">
-        <button @click="toggleActive(command)" class="command-button" :class="{active: command.active}"><span>{{ command.name }}</span></button>
-      </h3>
-      <p class="command__desc">{{ command.desc | twoOrphans }}</p>
-      <ul class="subcommand-list" v-show="command.active">
-        <li class="subcommand" v-for="subcommand in command.subcommands" :key="subcommand.id">
-          <template v-if="subcommand.args">
-            <div class="tooltip" :class="{active: subcommand.active}" :id="command.name+'-'+subcommand.name+'-tooltip'" role="tooltip">
-              <ul class="arg-list">
-                <li class="arg" v-for="arg in subcommand.args" :key="arg.id">
-                  <p class="arg__name">{{ arg.name }} <span>(<template v-if="arg.required">required</template><template v-else>optional</template>)</span></p>
-                  <p class="small-text" v-html="$options.filters.twoOrphans(arg.desc)+(arg.options?':':'')"></p>
-                  <ul class="option-list" v-if="arg.options">
-                    <li class="option small-text" v-for="option in arg.options" :key="option.id" v-html="$options.filters.twoOrphans(option)"></li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-            <p class="subcommand__name">
-              <span>{{ command.name }}</span> {{ subcommand.name }}
-              <button @click="toggleActive(subcommand)" class="args-button" :class="{active: subcommand.active}" :aria-describedby="command.name+'-'+subcommand.name+'-tooltip'">
-                +{{ subcommand.args.length }}
-              </button>
-            </p>
-            <p class="small-text">{{ subcommand.desc | twoOrphans }}</p>
-          </template>
-          <template v-else>
-            <p class="subcommand__name"><span>{{ command.name }}</span> {{ subcommand.name }}</p>
-            <p class="small-text">{{ subcommand.desc | twoOrphans }}</p>
-          </template>
-        </li>
-      </ul>
-    </template>
+    <ul v-if="command.subcommands" v-show="command.active" class="subcommand-list" :id="command.name+'-subcommands'">
+      <li class="subcommand" v-for="subcommand in command.subcommands" :key="subcommand.id">
+        <div v-if="subcommand.args" class="tooltip" :class="{active: subcommand.active}" :id="command.name+'-'+subcommand.name+'-tooltip'">
+          <cmdArgs :command="subcommand"/>
+        </div>
+
+        <p class="subcommand__name">
+          <span>{{ command.name }}</span> {{ subcommand.name }}
+          <button v-if="subcommand.args" @click="toggleActive(subcommand)" class="args-button" :class="{active: subcommand.active}" :aria-controls="command.name+'-'+subcommand.name+'-tooltip'" :aria-expanded="subcommand.active?'true':'false'" :title="subcommand.active?'Hide command arguments':'Show command arguments'" v-html="'+'+subcommand.args.length"></button>
+        </p>
+        <p class="small-text">{{ subcommand.desc | twoOrphans }}</p>
+      </li>
+    </ul>
   </li>
 </template>
 
 <script>
 import Vue from "vue";
+import cmdArgs from "./CmdArgs";
 
 export default {
+  components: {
+    cmdArgs
+  },
   props: {
     command: {
       type: Object,
--- src/components/Commands.vue
+++ src/components/Commands.vue
@@ -1,15 +1,12 @@
 <template>
   <main id="commands">
-    <div class="feature-section vertical-align vertical-align--middle">
-      <div class="vertical-align__content">
-        <div class="page-width page-padding">
-          <h1>GW2Bot Commands</h1>
-          <h2 class="paragraph">Type <code>/</code> to see a full list of commands</h2>
-          <p>Use <code>&#11134; Tab</code> to autocomplete commands with options<!--: <code>/key add</code>--></p>
-        </div>
-      </div>
-    </div>
     <div class="page-width page-padding">
+      <header>
+        <h1>GW2Bot Commands</h1>
+        <p><strong>Type <code>/</code> to see a full list of commands</strong></p>
+        <p>Use <code>&#11134; Tab</code> to autocomplete commands with options</p>
+      </header>
+
       <div class="flexbox">
         <div class="flexbox__item commands-column">
           <ul class="commands-list">
