46c51bae3d3031f5de23396c5eca64133b86c99e
Matt Bryant <brybrant@gmail.com>
2022-07-20 03:14:23 +0100

    Turned PayPal donate form + button into a link (much less HTML)

--- src/components/PatreonComponent.vue
+++ src/components/PatreonComponent.vue
@@ -5,22 +5,15 @@
 
       <span class="h2">Contribute to GW2Bot</span>
 
-      <p>GW2Bot will remain free to use and continues to run with kind donations and volunteer contributions.<br>If you enjoy using GW2Bot, please consider donating to fund future development!</p>
+      <p>GW2Bot will remain free to use and continues to run with kind donations and volunteer&nbsp;contributions.<br>If you enjoy using GW2Bot, please consider donating to fund future&nbsp;development!</p>
 
       <div class="flexbox">
         <div class="flexbox__item">
-          <a id="patreon" class="button button--large button--patreon" href="https://www.patreon.com/bePatron?u=9811418" target="_blank">Become a Patron</a>
+          <a class="button button--large button--patreon" href="https://www.patreon.com/bePatron?u=9811418" target="_blank">Become a Patron</a>
         </div>
 
         <div class="flexbox__item">
-          <form id="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
-            <input type="hidden" name="cmd" value="_s-xclick">
-            <input type="hidden" name="hosted_button_id" value="96N5SP5FYTTGJ">
-
-            <button class="button button--large button--paypal" name="submit" type="submit">Donate with PayPal</button>
-
-            <img class="sr-only" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" aria-hidden="true">
-          </form>
+          <a class="button button--large button--paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=96N5SP5FYTTGJ" target="_blank">Donate with PayPal</a>
         </div>
       </div>
     </div>
