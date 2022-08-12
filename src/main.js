7cb0390ee752d755a9c37c8c0f22c694d015ea15
Matt Bryant <brybrant@gmail.com>
2022-08-13 00:47:16 +0100

    Moved bot description to global variables to avoid code duplication.

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ Vue.prototype.$inviteLink = "https://discord.com/api/oauth2/authorize?client_id=
 
 Vue.prototype.$supportServerLink = "https://discord.gg/VyQTrwP";
 
+Vue.prototype.$botDescription = "GW2Bot is a free and open-source interactive software tool (&ldquo;bot&rdquo;) created for use exclusively with the <a class='external' href='https://discord.com/' target='_blank'>Discord</a> application, and is designed to provide helpful and convenient features that compliment <a class='external' href='https://www.guildwars2.com' target='_blank'>Guild&nbsp;Wars&nbsp;2</a> (<abbr>GW2</abbr>)&nbsp;gameplay. GW2Bot is licensed under the <a class='external' href='https://www.gnu.org/licenses/gpl-3.0.en.html'>GNU General Public Licence</a> with no warranty, and permission is granted to copy, distribute and/or modify <a class='external' href='https://github.com/Maselkov/GW2Bot'>the source code</a> on the basis that you grant the same freedoms offered by this licence to users of your&nbsp;copy.";
+
 Vue.filter("twoOrphans",function(text) {
   // Replace the last whitespace character with a non-breaking space to force a
   // minimum of two orphan words in a given block of text
